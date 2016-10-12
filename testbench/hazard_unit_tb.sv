b1e8027bca8112e6c0f9650f8f2adc10092874ef
Mitchel Bouma <mbouma@purdue.edu>
2016-10-12 16:49:56 -0400

    finished test bench

--- testbench/hazard_unit_tb.sv
+++ testbench/hazard_unit_tb.sv
@@ -31,13 +31,17 @@ module hazard_unit_tb;
   hazard_unit_if huif();
   pipeline_fetch_if pfif();
   pipeline_decode_if pdif();
+  datapath_cache_if dpif();
+  forwarding_unit_if fuif();
+  pipeline_memory_if pmif();
+  pipeline_execute_if peif();
+  
   // test program
-  test PROG (CLK, nRST, huif, pfif, pdif);
-
-  pipeline_fetch PF (CLK, nRST, pfif, huif);
-  pipeline_decode PD (CLK, nRST, pfif, pdif, huif);
-
-  hazard_unit HU (CLK, nRST, huif, pfif, pdif);
+  test PROG (CLK, nRST, huif, pfif, pdif, fuif);
+  //pipeline_fetch PF (CLK, nRST, pfif, huif);
+  //pipeline_decode PD (CLK, nRST, pfif, pdif, huif);
+  forwarding_unit FU (CLK, nRST, fuif, peif, pdif, pmif);
+  hazard_unit HU (CLK, nRST, huif, pfif, pdif, dpif);
 
 endmodule
 
@@ -46,7 +50,8 @@ program test(
   output logic nRST,
   hazard_unit_if huif,
   pipeline_fetch_if pfif,
-  pipeline_decode_if pdif
+  pipeline_decode_if pdif,
+  forwarding_unit_if fuif
 );
 
   parameter PERIOD = 10;
@@ -56,21 +61,48 @@ program test(
     @(posedge CLK);
 
     nRST = 1'b0;
+
     @(posedge CLK);
+
     nRST = 1'b1;
 
+    assign pdif.ID_mem2reg_OUT = 1;
+
+    assign pdif.ID_Instr_OUT[20:16] = 5'b11011;  
+
+    assign pfif.IF_Instr_OUT[25:21] = 5'b11011;
+    assign pfif.IF_Instr_OUT[20:16] = 5'b00111;
+
+    /*dpif.ihit = 1;
+    pfif.flush = 0;
+
     @(posedge CLK);
 
-    //pdif.ID_Instr_IN[20:16] = 5;
+    //ori $1, $zero, 0xF0
+    pfif.IF_Instr_IN = 32'h340100F0;
 
-    pfif.IF_Instr_IN = '1;
-    @(posedge CLK);
-    @(posedge CLK);
-    @(posedge CLK);
+    @(posedge CLK); 
+
+    //ori $2, $zero, 0x02
+    pfif.IF_Instr_IN = 32'h34020002;
+
+    @(posedge CLK); 
+
+    //add $3, $1, $2
+    pfif.IF_Instr_IN = 32'h00221820;
+
     @(posedge CLK);
 
-    pdif.ID_mem2reg_IN = 1;
+    //add $4, $3, $1
+    pfif.IF_Instr_IN = 32'h00612020;
 
+    @(posedge CLK);
+
+    //halt
+    pfif.IF_Instr_IN = 32'hFFFFFFFF;*/
+
+    @(posedge CLK);
+    @(posedge CLK);
 
   end
 
