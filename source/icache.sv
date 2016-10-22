1919a2ea5b5e641c1059d7433b74df1f98905995
Mitchel Bouma <mbouma@purdue.edu>
2016-10-21 21:02:22 -0400

    finished caches

---
+++ source/icache.sv
@@ -0,0 +1,80 @@
+// Alex Dunker
+// adunker@purdue.edu
+
+// icache
+
+`include "datapath_cache_if.vh"
+`include "cache_control_if.vh"
+
+`include "cpu_types_pkg.vh"
+
+import cpu_types_pkg::*;
+
+module icache (input logic CLK, nRST,datapath_cache_if dcif, caches_if cif);
+
+	//59 bits wide per row, 16 rows
+
+	logic [31:0] instruction;
+	logic [31:0] n_instruction;
+	logic [15:0][58:0] cacheValues, n_cacheValues;
+	logic [ITAG_W-1:0] tag;
+	logic [IIDX_W-1:0] index;
+	logic [ITAG_W-1:0] tagToCheck;
+	logic valid_chk;
+	logic same_tag;
+	word_t data_stored;
+
+	assign tag = dcif.imemaddr[31:6];
+	assign index = dcif.imemaddr[5:2];
+	assign tagToCheck = cacheValues[index][57:32];
+	assign valid_chk = cacheValues[index][58];
+	assign data_stored = cacheValues[index][31:0];
+	assign same_tag = (tagToCheck == tag);
+
+   	integer i;
+   	always_ff @(posedge CLK, negedge nRST) begin
+      	if (!nRST) begin
+        	instruction <= '0; 
+         	for (i=0;i<16;i++) begin
+            		cacheValues[i][58] <= 0;
+         	end
+      	end else begin
+         	instruction <= n_instruction;
+         	cacheValues <= n_cacheValues;
+      	end
+		end
+
+   	always_comb begin
+      		cif.iaddr = 0;
+      		dcif.imemload = 0;
+		cif.iREN = 0;
+		dcif.ihit = 0;
+      		n_instruction = instruction;
+      		n_cacheValues = cacheValues;
+
+      		if ((dcif.dmemREN == 0) && (dcif.dmemWEN == 0) && (dcif.halt == 0)) begin
+         		if(valid_chk && same_tag) begin 
+            			dcif.imemload = data_stored;
+            			dcif.ihit = 1;
+            			n_instruction = data_stored;
+			end else begin 
+            			cif.iREN = dcif.imemREN;
+            			cif.iaddr = dcif.imemaddr;
+            			if(cif.iwait == 0) begin
+					dcif.ihit = 1;
+               				n_cacheValues[index][58] = 1;
+               				n_cacheValues[index][57:32] = tag;
+               				n_cacheValues[index][31:0] = cif.iload;
+               				dcif.imemload = cif.iload;
+               				n_instruction = cif.iload;
+            			end else begin
+               				dcif.imemload = instruction;
+            			end
+         		end
+      		end
+  	end
+
+endmodule
+
+
+
---
+++ testbench/dcache_tb.sv
@@ -0,0 +1,169 @@
+// Alex Dunker
+// adunker@purdue.edu
+// icache Test Bench
+
+`include "cpu_types_pkg.vh"
+`include "datapath_cache_if.vh"
+`include "caches_if.vh"
+
+import cpu_types_pkg::*;
+
+`timescale 1 ns / 1 ns
+
+module dcache_tb;
+
+	parameter PERIOD = 10;
+	logic CLK = 0, nRST;
+	// clock
+	always #(PERIOD/2) CLK++;
+	// interface
+	caches_if cif ();
+	datapath_cache_if dcif ();
+
+	test PROG(CLK, nRST, dcif, cif);
+	dcache DC (CLK, nRST, dcif, cif);
+
+endmodule
+
+program test(
+	input logic CLK,
+	output logic nRST,
+	datapath_cache_if dcif,
+	caches_if cif
+);
+
+	initial begin
+		parameter PERIOD = 10;
+		parameter CPUID = 0;
+
+		cif.dwait = 1'b0;
+		cif.dload = 1'b0;
+
+		nRST = 1'b0;
+		#(PERIOD*1);
+		@(posedge CLK);
+		nRST = 1'b1;
+		#(PERIOD*1);
+
+		//load in something
+		dcif.dmemaddr = 32'h00000000;
+		dcif.dmemstore = 32'hballoons;
+		dcif.dmemREN = 1'b1;
+		dcif.dmemWEN = 1'b0;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'hffffffff;
+
+		#(PERIOD*1);
+		//load it into the other side
+		dcif.dmemaddr = 32'hffff0000;
+		dcif.dmemstore = 32'hballoons;
+		dcif.dmemREN = 1'b1;
+		dcif.dmemWEN = 1'b0;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'h0000ffff;
+
+		#(PERIOD*2);
+		#(PERIOD*2);
+		//read next index from RAM
+		dcif.dmemaddr = 32'b00000000000000000000000000001000;
+		dcif.dmemstore = 32'hbad1bad1;
+		dcif.dmemREN = 1'b1;
+		dcif.dmemWEN = 1'b0;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'h00000000;
+
+		dcif.dmemREN = 1'b0;
+		#(PERIOD*1);
+		//write to data already in cache
+		dcif.dmemaddr = 32'b00000000000000000000000000001000;
+		dcif.dmemstore = 32'hffff0000;
+		dcif.dmemREN = 1'b0;
+		dcif.dmemWEN = 1'b1;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'hbad1bad1;
+		dcif.dmemWEN = 1'b0;
+
+		#(PERIOD*2);
+		//consecutive read hits
+		dcif.dmemaddr = 32'b00000000000000000000000000001000;
+		dcif.dmemstore = 32'hffff0000;
+		dcif.dmemREN = 1'b1;
+		dcif.dmemWEN = 1'b0;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'hbad1bad1;
+
+		#(PERIOD*2);
+		dcif.dmemREN = 1'b0;
+		//write to data not in cache
+		dcif.dmemaddr = 32'b00000000000000000000000000010000;
+		dcif.dmemstore = 32'hf0faf0f0;
+		dcif.dmemREN = 1'b0;
+		dcif.dmemWEN = 1'b1;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'hbad1bad1;
+		#(PERIOD*2);
+
+
+		#(PERIOD*1);
+		dcif.dmemaddr = 32'b00000000000000000000000000000000;
+		dcif.dmemstore = 32'hbad1bad1;
+		dcif.dmemREN = 1'b1;
+		dcif.dmemWEN = 1'b0;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload[CPUID] = 32'hbad1bad1;
+
+		#(PERIOD*1);
+		dcif.dmemaddr = 32'hffff0000;
+		dcif.dmemstore = 32'hbad1bad1;
+		dcif.dmemREN = 1'b1;
+		dcif.dmemWEN = 1'b0;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'hbad1bad1;
+		#(PERIOD*1);
+		dcif.dmemREN = 1'b0;
+
+		#(PERIOD*1);
+		//eviction on read clean
+		dcif.dmemaddr = 32'b10101010101000000000000000000000;
+		dcif.dmemstore = 32'hbad1bad1;
+		dcif.dmemREN = 1'b1;
+		dcif.dmemWEN = 1'b0;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'h10101010;
+
+		#(PERIOD*1);
+		//eviction on write clean
+		dcif.dmemaddr = 32'b01010101010100000000000000000000;
+		dcif.dmemstore = 32'hFEEDBEEF;
+		dcif.dmemREN = 1'b0;
+		dcif.dmemWEN = 1'b1;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'hbad1bad1;
+		#(PERIOD*2); // 
+
+		#(PERIOD*2);
+		dcif.dmemREN = 1'b0;
+		//write to data not in cache
+		dcif.dmemaddr = 32'b00001111000000000000000000010000;
+		dcif.dmemstore = 32'hf0fffff0;
+		dcif.dmemREN = 1'b0;
+		dcif.dmemWEN = 1'b1;
+		dcif.halt = 1'b0;
+		cif.dwait = 1'b0;
+		cif.dload = 32'hbad1bad1;
+		#(PERIOD*2);
+
+
+		#(PERIOD*5);
+	end
+endprogram
