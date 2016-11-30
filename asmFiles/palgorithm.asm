f59293b1e28d01fa692e3e4a32012a994353b5fa
Alex Dunker <adunker@purdue.edu>
2016-11-30 14:47:30 -0500

    Palgorithm updates

--- asmFiles/palgorithm.asm
+++ asmFiles/palgorithm.asm
@@ -7,45 +7,45 @@
 #*****************************************************************************#
 
 	org 	0x000 				# first processor
-	ori		$s0, $0, 0			# values to generate
-	ori 	$s1, $0, 0			# the bufferhead
-	ori		$s2, $0, 0			# buffer size
-	ori		$s3, $0, 10			# max buffer size
-	ori		$s4, $0, 0			# value
-	ori		$s5, $0, 0xbabe		# seed
-	ori		$s6, $0, 40			# buffer head
-	ori		$s7, $0, 256
+	ori		$s0, $0, 256		# total num
+	ori		$s1, $0, 0			# values to generate
+	ori		$s2, $0, 0			# value
+	ori		$s3, $0, 0xbabe	# seed
+	ori 	$s4, $0, 0			# the bufferhead
+	ori		$s5, $0, 0			# buffer size
+	ori		$s6, $0, 10		# max buffer size
+	ori		$s7, $0, 40		# buffer head end
 
 producer:
-	lw		$s2, buffSZ($0)		# load size
-	beq		$s2, $s3, producer  # at max size, do nothing
-	beq		$s4, $s7, finish_p0	# finish processor 0 if 256 have been generated
+	lw		$s5, buffSZ($0)		# load size
+	beq		$s5, $s6, producer  # at max size, do nothing
+	beq		$s2, $s0, finish_p0	# finish processor 0 if 256 have been generated
 	ori 	$a0, $0, l4			# move lock to argument register
 	jal 	lock				# try to acquire lock
-	lw 		$s2, buffSZ($0)		# load size
-	addiu	$s2, $s2, 1			# increment
-	sw		$s2, buffSZ($0)		# store increment
-	ori		$a0, $s5, 0			# set seed
+	lw 		$s5, buffSZ($0)		# load size
+	addiu	$s5, $s5, 1			# increment
+	sw		$s5, buffSZ($0)		# store increment
+	ori		$a0, $s3, 0			# set seed
 	jal 	crc32				# generate random Numerator
-	addiu	$t0, $s1, buffer 	# buffer head location
+	addiu	$t0, $s4, buffer 	# buffer head location
 	sw		$v0, 0($t0)			# store on ring buffer 
-	ori		$s5, $v0, 0x0000	# update seed
-	addiu	$s1, $s1, 0x0004	# increment the head
+	ori		$s3, $v0, 0x0000	# update seed
+	addiu	$s4, $s4, 0x0004	# increment the head
 	jal		head_reset
 	ori 	$a0, $0, l4
 	jal 	unlock				# release lock 
 	ori 	$t0, $0, 256		# max to generate
-	addiu 	$s4, $s4, 1			# increment
+	addiu 	$s2, $s2, 1			# increment
 	ori 	$t2, $0, 256
-	beq 	$s4, $t0, finish_p0 # finish
+	beq 	$s2, $t0, finish_p0 # finish
 	j 		producer
 
 head_reset:
-	beq		$s1, $s6, head_reset2
+	beq		$s4, $s7, head_reset2
 	jr 		$ra 				# return to caller
 
 head_reset2:
-	ori		$s1, $0, 0
+	ori		$s4, $0, 0
 	jr		$ra 				# return to caller
 
 finish_p0:
@@ -58,12 +58,12 @@ finish_p0:
 #*****************************************************************************#
 
 	org		0x200 				#second processor
-	ori 	$s2, $0, 40
-	ori		$s3, $0, 1
-	ori		$s4, $0, 0			# buffer tail
-	ori		$s5, $0, 0xFFFF		# min
-	ori		$s6, $0, 0x0000		# max
-	ori 	$s7, $0, 0x0000		# sum
+	ori 	$s5, $0, 40
+	ori		$s6, $0, 1
+	ori		$s2, $0, 0			# buffer tail
+	ori		$s3, $0, 0xFFFF		# min
+	ori		$s7, $0, 0x0000		# max
+	ori 	$s0, $0, 0x0000		# sum
 
 consumer:
 	lw		$t3, buffSZ($0)
@@ -74,31 +74,31 @@ consumer:
 	ori		$t0, $0, 1			
 	subu 	$t3, $t3, $t0		# decrease size
 	sw		$t3, buffSZ($0) 	# store value
-	addiu	$t4, $s4, buffer 	# tail location
+	addiu	$t4, $s2, buffer 	# tail location
 	lw 		$t1, 0($t4) 		# load val from tail
 	ori 	$a1, $t1, 0			# cur val 
 	andi 	$a1, $a1, 0xFFFF 
-	addiu 	$s4, $s4, 4			# increment the tail
+	addiu 	$s2, $s2, 4			# increment the tail
 	jal		tail_reset 
-	ori 	$a0, $s5, 0			# setup argument
+	ori 	$a0, $s3, 0			# setup argument
 	jal		min 				# get the minimum
-	ori 	$s5, $v0, 0			# put the sum in the register
-	ori 	$a0, $s6, 0			# setup argument
+	ori 	$s3, $v0, 0			# put the sum in the register
+	ori 	$a0, $s7, 0			# setup argument
 	jal		max 				# get the max
-	ori 	$s6, $v0, 0			# pull the returned result
-	addu 	$s7, $s7, $a1 		# sum
+	ori 	$s7, $v0, 0			# pull the returned result
+	addu 	$s0, $s0, $a1 		# sum
 	ori 	$a0, $0, l4			# move lock to argumetn
 	jal		unlock				# release the lock
 
 	j 		consumer			# DO IT ALL AGAIN
 
 finish_p1:
-	sw 		$s5, v_min($0)		# min value
-	sw		$s6, v_max($0)		# max value
+	sw 		$s3, v_min($0)		# min value
+	sw		$s7, v_max($0)		# max value
 
-	srl 	$s7, $s7, 4
-	srl 	$s7, $s7, 4			# user lower 16 bits
-	sw 		$s7, v_avg($0)		# store the value
+	srl 	$s0, $s0, 4
+	srl 	$s0, $s0, 4			# user lower 16 bits
+	sw 		$s0, v_avg($0)		# store the value
 
 	halt
 
@@ -108,11 +108,11 @@ p1_check:
 	j 		finish_p1
 
 tail_reset:
-	beq 	$s4, $s2, tail_reset2
+	beq 	$s2, $s5, tail_reset2
 	jr 		$ra 				# return to caller
 
 tail_reset2:
-	ori 	$s4, $0, 0
+	ori 	$s2, $0, 0
 	jr 		$ra 				# return to caller
 
 # pass in an address to lock function in argument register 0
